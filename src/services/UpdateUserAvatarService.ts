0313842b011a2d302b97210dde9baa8f855935ca
Fabio Picoli <fabio.picoli@totvs.com.br>
2020-05-20 07:22:50 -0300

    Global error handling

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -1,8 +1,10 @@
 import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
-
 import uploadConfig from '../config/upload';
+
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -17,7 +19,7 @@ class UpdateUserAvatarService {
     const user = await userRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only authenticated users can change avatar.');
+      throw new AppError('Only authenticated users can change avatar.', 401);
     }
 
     if (user.avatar) {
@@ -28,11 +30,7 @@ class UpdateUserAvatarService {
       const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
       if (userAvatarFileExists) {
-        try {
-          await fs.promises.unlink(userAvatarFilePath);
-        } catch (err) {
-          throw new Error('Avatar not founded.');
-        }
+        await fs.promises.unlink(userAvatarFilePath);
       }
     }
 
